53620096e3218c13699869a00c1c8db23a0025b9
Asher Lieber <asherlie@buffalo.edu>
2017-05-26 19:55:56 -0400

    added article fetcher

---
+++ articles.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import time
+from datetime import date, timedelta
+
+base = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?%s'
+def get_articles(n):
+    articles = []
+    today = date.today()
+    day = 0
+
+    while True:
+        td = today - timedelta(days=day)
+        day += 1
+        start_date = end_date = str(td).replace('-', '')
+        params = { 'facet_field': 'day_of_week', 'begin_date': start_date, 'end_date': end_date, 'api-key': '________________________________' }
+        res = requests.get(base, params).json()['response']['docs']
+        for article in res:
+            articles.append([article['headline']['main'] + article['snippet'].strip('....') + article['lead_paragraph'], article['web_url']])
+            if len(articles) == n: break
+            time.sleep(.9) #bc the usage limit of the api is 1 call/second
+        if len(articles) == n: break
+    return articles

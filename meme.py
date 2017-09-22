8679333998cbac24e1651d1e06c644be3cf315c7
asherlie <lieber.asher@gmail.com>
2017-09-22 15:12:52 -0400

    removed old functions

--- meme.py
+++ meme.py
@@ -11,23 +11,23 @@ class Meme:
         for i in reference['data']['memes']:
                 ref[i['name']] = i['id'] 
 
-        def to_cam(self, wrd):
-                new_str = ''
-                for i in wrd.split(' '):
-                        for ltr in range(len(i)):
-                                if ltr == 0: new_str += (' ' + i[ltr].upper())
-                                else: new_str += i[ltr]
-                return new_str[1:len(new_str)]
         def meme_to_id(self, meme_name):
-                if self.to_cam(meme_name) in self.ref: return self.ref[self.to_cam(meme_name)]
+                def to_cam(wrd):
+                        new_str = ''
+                        for i in wrd.split(' '):
+                                for ltr in range(len(i)):
+                                        if ltr == 0: new_str += (' ' + i[ltr].upper())
+                                        else: new_str += i[ltr]
+                        return new_str[1:len(new_str)]
+                if to_cam(meme_name) in self.ref: return self.ref[to_cam(meme_name)]
                 return "you fucked up"  
 
 
         def gen_meme(self, meme_id, top, bottom):
                 parameters = {
                                 'template_id': meme_id,
-                                'username': 'ubhacking2016',
-                                'password': 'ubhacking2016',
+                                'username': '____',
+                                'password': '____',
                                 'text0': top, #top text for meme
                                 'text1': bottom, #bottom text for meme
                                 }
@@ -47,54 +47,6 @@ class Meme:
                 ret.append(tuple(tmp_meme))
             return ret
 
-        def parse_delims(self, strng):
-                def real_split(st):
-                        return True
-                        return st.split('&&')[1] != '"]'
-                if strng.find('&&') != -1 and real_split(strng): to_parse = strng.split('&&')[random.randrange(len(strng.split('&&'))-1)]
-                else: to_parse = strng
-                m_type = to_parse.split('^^')[0].split('|')
-                top = []; bot = []
-                top_text = ''; bot_text = ''
-                if to_parse.find('@@') != -1:
-                        for i in to_parse.split('^^')[1].split('@@'):
-                                top.append(i.split('#%')[0])
-                                bot.append(i.split('#%')[1])
-                else:
-                        top.append(to_parse.split('^^')[1].split('#%')[0])
-                        bot.append(to_parse.split('^^')[1].split('#%')[1])
-                # m_type is a [] bc first has pos sentiment, second neg
-                # this returns ([pos_sent, neg_sent], ([pos_top, neg_top], [pos_bot, neg_bot]))
-                # TODO: there will be cases where len(m_type) > 1 and len(top) == 1 or len(bot) == 1
-                # handle these. if len of top or bot is 2 and so is m_type,
-                # choose the corresponding top or bot. if either doenst have both, use the only one
-                return (m_type, (top, bot))
-
-        def select_top_bot(self, mm):
-                m_type = mm[0]
-                top = mm[1][0]
-                bot = mm[1][1]
-                if len(m_type) != 1:
-                        sent = requests.post('http://text-processing.com/api/sentiment/', 'text=' + strng).json()
-                        if sent['probability']['pos'] > sent['probability']['neg']: 
-                            m_type = m_type[0]
-                            top_text = top[0] #can just do this bc it defaults to [0] anyway
-                            bot_text = bot[0] #and [0] is positive option
-                        else: 
-                            m_type = m_type[1]
-                            if len(top) == 2:
-                                top_text = top[1]
-                                bot_text = bot[1]
-                            else:
-                                top_text = top[0]
-                                bot_text = bot[0]
-                else:
-                    m_type = m_type[0]
-                    top_text = top[0]
-                    bot_text = bot[0]
-                return (m_type, (top_text, bot_text))
-            
-
         def memes_from_f(self, delim_file):
             with open(delim_file, 'r') as f:
                 fi = f.read()
@@ -113,53 +65,8 @@ class Meme:
                     m_type = mtu[0][0]
                 else:
                     m_type = mtu[0][1]
-            print(m_type)    
             return self.gen_meme(self.meme_to_id(m_type), mtu[1][0], mtu[1][1])
 
         def sent_pos(self, strng):
                 sent = requests.post('http://text-processing.com/api/sentiment/', 'text=' + strng).json()
                 return sent['probability']['pos'] > sent['probability']['neg']
-
-                
-        def spit(self, strng):
-                a = os.popen('./meme "' + strng + '"').read().encode('ascii', 'ignore').decode().strip('&&[()]\n')
-                if a.find('&&') != -1: to_parse = a.split('&&')[random.randrange(len(a.split('&&'))-1)]
-                else: to_parse = a
-                m_type = to_parse.split('^^')[0].split('|')
-
-                top = []; bot = []
-                top_text = ''; bot_text = ''
-                if to_parse.find('@@') != -1:
-                        for i in to_parse.split('^^')[1].split('@@'):
-                                top.append(i.split('#%')[0])
-                                bot.append(i.split('#%')[1])
-                else:
-                        top.append(to_parse.split('^^')[1].split('#%')[0])
-                        bot.append(to_parse.split('^^')[1].split('#%')[1])
-
-                if len(m_type) != 1:
-                        sent = requests.post('http://text-processing.com/api/sentiment/', 'text=' + strng).json()
-                        if sent['probability']['pos'] > sent['probability']['neg']: 
-                            m_type = m_type[0]
-                            top_text = top[0] #can just do this bc it defaults to [0] anyway
-                            bot_text = bot[0] #and [0] is positive option
-                        else: 
-                            m_type = m_type[1]
-                            if len(top) == 2:
-                                top_text = top[1]
-                                bot_text = bot[1]
-                            else:
-                                top_text = top[0]
-                                bot_text = bot[0]
-                else:
-                    m_type = m_type[0]
-                    top_text = top[0]
-                    bot_text = bot[0]
-                mem = self.gen_meme(self.meme_to_id(m_type), top_text, bot_text).json()
-                if 'data' in mem:
-                    return mem['data']['url']
-                else: 
-                    print(m_type)
-                    return "incorrect meme template"
-if __name__ == '__main__':
-        print(Meme().spit(sys.argv[1]))
